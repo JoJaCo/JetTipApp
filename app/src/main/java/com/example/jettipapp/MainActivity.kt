6502e43694dadaf3429c8c7ae4797a857bd9957b
JoJACO <contrerasjorge226@gmail.com>
2023-08-24 13:31:48 -0700

    finsiehd up on the text box, still need to look into the enable issue in Components.kt.

--- app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -14,17 +14,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jettipapp.components.InputField
 import com.example.jettipapp.ui.theme.JetTipAppTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
@@ -67,32 +73,60 @@ fun TopHeader(totalPerPerson: Double = 133.0) {
             .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
         color = Color(0xFFA1E2EB)
     ) {
-        Column(modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
 
             val total = "%.2f".format(totalPerPerson)
 
-            Text(text = "Total Per Person",
-            style = MaterialTheme.typography.displaySmall)
-            Text(text = "$$total",
-            style = MaterialTheme.typography.displaySmall,
-            fontWeight = FontWeight.Bold)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.displaySmall
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.displaySmall,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
-fun MainContent(){
-    Surface(modifier = Modifier
-        .padding(2.dp)
-        .fillMaxWidth(),
-    shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
         Column() {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enable = true,
+                isSingleLine = true,
+                onAction = KeyboardActions{
+                    if (!validState) return@KeyboardActions
+                    //ToDo- onvaluechagned
+
+                    keyboardController?.hide()
+                }
+            )
 
 
         }
@@ -100,11 +134,6 @@ fun MainContent(){
     }
 
 }
-
-
-
-
-
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/jettipapp/components/Components.kt
+++ app/src/main/java/com/example/jettipapp/components/Components.kt
@@ -1,5 +1,6 @@
 package com.example.jettipapp.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -17,6 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,6 +38,7 @@ fun InputField(
     OutlinedTextField(
         value = valueState.value, onValueChange = { valueState.value = it },
         label = { Text(text = labelId) },
+        enable = enable,
         leadingIcon = {
             Icon(
                 imageVector = Icons.Rounded.AttachMoney,
@@ -44,7 +48,8 @@ fun InputField(
         singleLine = isSingleLine,
         textStyle = TextStyle(fontSize = 18.sp,
             color = MaterialTheme.colorScheme.onBackground),
-       // enable = enable,
+        modifier = modifier.padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
+
         keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
         keyboardActions = onAction
 
